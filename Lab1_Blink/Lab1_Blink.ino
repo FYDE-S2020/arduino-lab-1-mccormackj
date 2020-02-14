541c412b54d56c86b5f866fb702ada87a093d683
Johanna McCormack <mccormackj@utexas.edu>
2020-02-14 12:40:06 -0600

    Analyzer Reports

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -21,25 +21,37 @@
 
   http://www.arduino.cc/en/Tutorial/Blink
 */
-#define LED 2
+
+#define LED_PIN 2
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void timedBlink(int t){
-  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(t);                       // wait for a second
-  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
   delay(t);       
 }
-
+void dimmer(int freq, int duty){
+  int period, onTime, offTime;
+  period = 1000/freq;
+  onTime = period*duty/100;
+  offTime = period-onTime;
+  digitalWrite(LED_PIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_PIN, LOW);
+  delay(offTime);
+}
 // the loop function runs over and over again forever
 void loop() {
-  timedBlink(250);
-  timedBlink(500);
-  timedBlink(1000);
+    for (int i=0; i<=100; i++){
+    dimmer(100, i);
+  }
+  for (int i=100; i>=0; i--){
+    dimmer(100, i);
+  }
 }
